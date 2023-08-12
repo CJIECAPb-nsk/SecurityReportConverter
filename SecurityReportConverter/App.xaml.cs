8da1b35507d4676bfead035b86a0fdd7809c12b4
cjiecapb <anton.bezkapustin@gmail.com>
2023-08-12 16:28:53 +0700

    Add logging

--- SecurityReportConverter/App.xaml.cs
+++ SecurityReportConverter/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using Serilog;
 
 namespace SecurityReportConverter
 {
@@ -13,5 +15,19 @@ namespace SecurityReportConverter
 	/// </summary>
 	public partial class App : Application
 	{
+		public App()
+		{
+			Current.DispatcherUnhandledException += App_UnhandledException;
+
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.WriteTo.File("logs\\application.log", rollingInterval: RollingInterval.Day)
+				.CreateLogger();
+		}
+
+		void App_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			Log.Error(e.Exception, "Непредвиденная ошибка.");
+		}
 	}
 }
